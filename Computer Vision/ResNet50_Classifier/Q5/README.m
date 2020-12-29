cffa903598c77cb047ec3abad4bc24043e3da8c2
Junbin Zhang <34506631+wolf-bailang@users.noreply.github.com>
2020-12-30 05:49:51 +0800

    Add files via upload

---
+++ Computer Vision/ResNet50_Classifier/Q5/README.m
@@ -0,0 +1,4 @@
+预训练模型　　https://drive.google.com/file/d/1YPyxmnkbEb9ERBFJxC83a7fwlcvxq1f9/view?usp=sharing
+best模型　　https://drive.google.com/file/d/1-yxp27cEoRC83QinJhxx9dXkcMT5EQFe/view?usp=sharing
+
+colab   https://drive.google.com/drive/folders/1uhmW9vQv7zvP88FnFvq7D0HVpS019F4o?usp=sharing
